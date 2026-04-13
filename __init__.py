cf1d0f58edea6a9728d6d5ea94cabbb02554efd2
As-419 <aissatousene1712@gmail.com>
2026-04-13 22:49:36 +0000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr  1 12:53:09 2026
+Created on Wed Apr  1 13:11:57 2026
 
 @author: Sene
 """
 
-from .string_utils import inverser_chaine, compter_mots
-
-__all__ = ["inverser_chaine", "compter_mots"]
---
+++ string_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 13:12:39 2026
+
+@author: Sene
+"""
+
+def inverser_chaine(chaine):
+    return chaine[ : :-1]
+
+def compter_mots(chaine):
+    return len(chaine.split())
